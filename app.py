48cc33322677672e39776dd8697bb38b2ad7b2d3
Deven Kalra <deven@kalra.com>
2020-06-03 23:52:22 -0700

    Added point line

--- app.py
+++ app.py
@@ -2,72 +2,101 @@ from tkinter import Tk, Canvas, Frame, BOTH, Menu, Toplevel, Button
 from tkcolorpicker import askcolor
 from mygraphics import Point, Graphics
 
+class MenuFunctions:
+    def __init__(self, root, graphics):
+        self.root = root
+        self.graphics = graphics
 
-def line(root, graphics):
-    def _line():
-        graphics.line(Point(0, 0), Point(100, 100))
-    return _line
+    def line(self):
+        def _line():
+            self.graphics.line(Point(0, 0), Point(100, 100))
+        return _line
 
-def select_color(root, graphics):
-    def _select_color():
-        color = askcolor((255, 255, 0), root)
-        graphics.set_color(color[1])
-    return _select_color
-
-
-def select_fill_color(root, graphics):
-    def _select_color():
-        color=  askcolor((255, 255, 0), root)
-        graphics.set_fill_color(color[1])
-    return _select_color
+    def select_color(self):
+        def _select_color():
+            color = askcolor((255, 255, 0), self.root)
+            self.graphics.set_color(color[1])
+        return _select_color
 
 
-def square(root, graphics):
-    def _square():
-        graphics.rectangle(Point(10, 10), Point(100, 100))
-    return _square
+    def select_fill_color(self):
+        def _select_color():
+            color=  askcolor((255, 255, 0), self.root)
+            self.graphics.set_fill_color(color[1])
+        return _select_color
 
 
-def donothing(root, graphics):
-    def _donothing():
-        filewin = Toplevel(root)
-        button = Button(filewin, text="Do nothing button")
-        button.pack()
-    return _donothing
+    def square(self):
+        def _square():
+            self.graphics.rectangle(Point(10, 10), Point(100, 100))
+        return _square
 
-def quit(root=None, graphics=None):
-    def _quit():
-        root.destroy()
-    return _quit
+
+    def donothing(self):
+        def _donothing():
+            filewin = Toplevel(self.root)
+            button = Button(filewin, text="Do nothing button")
+            button.pack()
+        return _donothing
+
+    def point_line(self):
+        def _point_line():
+            self.first_point = None
+            self.second_point = None
+
+            def motion2(event):
+                print("Mouse position: (%s %s)" % (event.x, event.y))
+                self.second_point = Point(event.x, event.y)
+                self.graphics.line(self.first_point, self.second_point)
+                return
+
+            def motion1(event):
+                global first_point
+                print("Mouse position: (%s %s)" % (event.x, event.y))
+                self.first_point = Point(event.x, event.y)
+                self.root.bind('<Button-1>', motion2)
+                return
+
+            self.root.bind('<Button-1>', motion1)
+        return _point_line;
+
+
+
+
+    def quit(self):
+        def _quit():
+            self.root.destroy()
+        return _quit
 
 def create_menu(root, graphics):
 
-
+    menu = MenuFunctions(root, graphics)
     menubar = Menu(root)
     filemenu = Menu(menubar, tearoff=0)
-    filemenu.add_command(label="New", command=donothing)
-    filemenu.add_command(label="Open", command=donothing)
-    filemenu.add_command(label="Save", command=donothing)
-    filemenu.add_command(label="Save as...", command=donothing)
+    filemenu.add_command(label="New", command=menu.donothing())
+    filemenu.add_command(label="Open", command=menu.donothing())
+    filemenu.add_command(label="Save", command=menu.donothing())
+    filemenu.add_command(label="Save as...", command=menu.donothing())
     filemenu.add_command(label="Close", command=quit)
 
     filemenu.add_separator()
 
-    filemenu.add_command(label="Exit", command=quit(root, graphics))
+    filemenu.add_command(label="Exit", command=menu.quit())
     menubar.add_cascade(label="File", menu=filemenu)
 
     editmenu = Menu(menubar, tearoff=0)
 
-    editmenu.add_command(label="Line", command=line(root, graphics))
-    editmenu.add_command(label="Square", command=square(root, graphics))
-    editmenu.add_command(label="Select Color", command=select_color(root, graphics))
-    editmenu.add_command(label="Select Fill Color", command=select_fill_color(root, graphics))
+    editmenu.add_command(label="Line", command=menu.line())
+    editmenu.add_command(label="Point Line", command=menu.point_line())
+    editmenu.add_command(label="Square", command=menu.square())
+    editmenu.add_command(label="Select Color", command=menu.select_color())
+    editmenu.add_command(label="Select Fill Color", command=menu.select_fill_color())
 
 
     menubar.add_cascade(label="Shapes", menu=editmenu)
     helpmenu = Menu(menubar, tearoff=0)
-    helpmenu.add_command(label="Help Index", command=donothing)
-    helpmenu.add_command(label="About...", command=donothing)
+    helpmenu.add_command(label="Help Index", command=menu.donothing())
+    helpmenu.add_command(label="About...", command=menu.donothing())
     menubar.add_cascade(label="Help", menu=helpmenu)
 
     root.config(menu=menubar)
